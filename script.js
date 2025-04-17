1e230b1fc4f0b3e45dff660d927f1bd0fa8abe63
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-17 13:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function validEmail(str) {
-  //your JS code here.
+function validEmail(s) {
+	let regex = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+	return regex.test(s);
 }
 
 // Do not change the code below.
+
+
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
